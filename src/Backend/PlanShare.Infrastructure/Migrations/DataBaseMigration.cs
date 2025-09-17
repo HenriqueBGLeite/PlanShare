c6512d6fe79b33247b11cffe0a488346e6b43ce6
HenriqueBGLeite <henriquecebrac37@gmail.com>
2025-09-17 10:47:29 -0300

    Add project files.

---
+++ src/Backend/PlanShare.Infrastructure/Migrations/DataBaseMigration.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using FluentMigrator.Runner;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MySqlConnector;
+using PlanShare.Infrastructure.DataAccess;
+
+namespace PlanShare.Infrastructure.Migrations;
+
+public static class DataBaseMigration
+{
+    public static void Migrate(string connectionString, IServiceProvider serviceProvider)
+    {
+        EnsureDatabaseCreatedForMySql(connectionString);
+
+        MigrateDatabase(serviceProvider);
+    }
+
+    private static void EnsureDatabaseCreatedForMySql(string connectionString)
+    {
+        var connectionStringBuilder = new MySqlConnectionStringBuilder(connectionString);
+
+        var databaseName = connectionStringBuilder.Database;
+
+        connectionStringBuilder.Remove("Database");
+
+        using var dbConnection = new MySqlConnection(connectionStringBuilder.ConnectionString);
+
+        dbConnection.Execute($"CREATE DATABASE IF NOT EXISTS {databaseName}");
+    }
+
+    private static void MigrateDatabase(IServiceProvider serviceProvider)
+    {
+        var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+        runner.ListMigrations();
+
+        runner.MigrateUp();
+    }
+}
