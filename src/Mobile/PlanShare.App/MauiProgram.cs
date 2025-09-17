8fce971f4a3ad77cee0bb6f456d8424eed0b81c1
HenriqueBGLeite <henriquecebrac37@gmail.com>
2025-09-17 20:57:12 -0300

    Feat: Navegando entre telas

--- src/Mobile/PlanShare.App/MauiProgram.cs
+++ src/Mobile/PlanShare.App/MauiProgram.cs
@@ -1,24 +1,35 @@
 ﻿using PlanShare.App.Constants;
+using PlanShare.App.Navigation;
+using PlanShare.App.Views.Pages.Login.DoLogin;
+using PlanShare.App.Views.Pages.User.Register;
 
-namespace PlanShare.App
+namespace PlanShare.App;
+
+public static class MauiProgram
 {
-    public static class MauiProgram
+    public static MauiApp CreateMauiApp()
     {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("Raleway-Regular.ttf", FontFamily.MAIN_FONT_REGULAR);
-                    fonts.AddFont("Raleway-Black.ttf", FontFamily.MAIN_FONT_BLACK);
-                    fonts.AddFont("Raleway-Thin.ttf", FontFamily.MAIN_FONT_THIN);
-                    fonts.AddFont("WorkSans-Regular.ttf", FontFamily.SECONDARY_FONT_REGULAR);                    
-                    fonts.AddFont("WorkSans-Black.ttf", FontFamily.SECONDARY_FONT_BLACK);
-                });
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .AddPages()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("Raleway-Regular.ttf", FontFamily.MAIN_FONT_REGULAR);
+                fonts.AddFont("Raleway-Black.ttf", FontFamily.MAIN_FONT_BLACK);
+                fonts.AddFont("Raleway-Thin.ttf", FontFamily.MAIN_FONT_THIN);
+                fonts.AddFont("WorkSans-Regular.ttf", FontFamily.SECONDARY_FONT_REGULAR);                    
+                fonts.AddFont("WorkSans-Black.ttf", FontFamily.SECONDARY_FONT_BLACK);
+            });
 
-            return builder.Build();
-        }
+        return builder.Build();
+    }
+
+    private static MauiAppBuilder AddPages(this MauiAppBuilder appBuilder)
+    {
+        Routing.RegisterRoute(RoutePages.LOGIN_PAGE, typeof(DoLoginPage));
+        Routing.RegisterRoute(RoutePages.USER_REGISTER_ACCOUNT_PAGE, typeof(RegisterUserAccountPage));
+
+        return appBuilder;
     }
 }
---
+++ src/Mobile/PlanShare.App/Navigation/RoutePages.cs
@@ -0,0 +1,10 @@
+﻿namespace PlanShare.App.Navigation;
+
+public class RoutePages
+{
+    public const string ONBOARDIN_PAGE = "OnBoardingPage";
+
+    public const string LOGIN_PAGE = "DoLoginPage";
+
+    public const string USER_REGISTER_ACCOUNT_PAGE = "RegisterUserAccountPage";
+}
