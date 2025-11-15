36e04e23044b53fdb5564d76b0816ea28736d783
HenriqueBGLeite <henriquecebrac37@gmail.com>
2025-11-15 18:39:09 -0300

    feat: implementando validação de idioma das mensagens

--- tests/Backend/WebApi.Tests/User/Register/RegisterUserTests.cs
+++ tests/Backend/WebApi.Tests/User/Register/RegisterUserTests.cs
@@ -1,13 +1,19 @@
 ﻿using CommonTestUtilities.Requests;
+using PlanShare.Domain.Extensions;
+using PlanShare.Exceptions;
 using Shouldly;
+using System.Globalization;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text.Json;
+using WebApi.Tests.InlineData;
 
 namespace WebApi.Tests.User.Register;
 
 public class RegisterUserTests : IClassFixture<CustomWebApplicationFactory>
 {
+    private const string BaseUrl = "/users";
     private readonly HttpClient _httpClient;
 
     public RegisterUserTests(CustomWebApplicationFactory factory)
@@ -22,17 +28,49 @@ public class RegisterUserTests : IClassFixture<CustomWebApplicationFactory>
         var request = RequestRegisterUserBuilder.Build();
 
         // Action
-        var response = await _httpClient.PostAsJsonAsync("/users", request);
+        var response = await _httpClient.PostAsJsonAsync(BaseUrl, request);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Created);
 
         using var responseBody = await response.Content.ReadAsStreamAsync();
 
         var document = await JsonDocument.ParseAsync(responseBody);
 
         // Assert
-        response.StatusCode.ShouldBe(HttpStatusCode.Created);
-
         document.RootElement.GetProperty("id").GetGuid().ShouldNotBe(Guid.Empty);
         document.RootElement.GetProperty("name").GetString().ShouldBe(request.Name);
         document.RootElement.GetProperty("tokens").GetProperty("accessToken").GetString().ShouldNotBeNullOrEmpty();
+    }
+
+    [Theory]
+    [ClassData(typeof(CultureInlineDataTest))]
+    public async Task Error_Name_Empty(string culture)
+    {
+        // Arrange
+        var request = RequestRegisterUserBuilder.Build();
+        request.Name = string.Empty;
+
+        _httpClient.DefaultRequestHeaders.AcceptLanguage.Clear();
+        _httpClient.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue(culture));
+
+        // Action
+        var response = await _httpClient.PostAsJsonAsync(BaseUrl, request);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+
+        using var responseBody = await response.Content.ReadAsStreamAsync();
+
+        var document = await JsonDocument.ParseAsync(responseBody);
+
+        // Assert
+        var errors = document.RootElement.GetProperty("errors").EnumerateArray();
+
+        var expectedMessage = ResourceMessagesException.ResourceManager.GetString("NAME_EMPTY", new CultureInfo(culture));
+
+        errors.ShouldSatisfyAllConditions(errors =>
+        {
+            errors.Count().ShouldBe(1);
+            errors.ShouldContain(error => error.GetString().NotEmpty() && error.GetString()!.Equals(expectedMessage));
+        });
     }
 }
