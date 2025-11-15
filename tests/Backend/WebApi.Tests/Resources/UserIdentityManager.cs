48594bf32a207c664cfb935d3ce0b4c8a8defba1
HenriqueBGLeite <henriquecebrac37@gmail.com>
2025-11-15 19:21:32 -0300

    feat: iniciando o banco com usuarios para testar metodo GetProfile

---
+++ tests/Backend/WebApi.Tests/Resources/UserIdentityManager.cs
@@ -0,0 +1,22 @@
+﻿using PlanShare.Domain.Dtos;
+
+namespace WebApi.Tests.Resources;
+
+public class UserIdentityManager
+{
+    private readonly PlanShare.Domain.Entities.User _user;
+    private readonly string _password;
+    private readonly TokensDto _tokens;
+
+
+    public UserIdentityManager(PlanShare.Domain.Entities.User user, string password, TokensDto tokens)
+    {
+        _user = user;
+        _password = password;
+        _tokens = tokens;
+    }
+
+    public string GetPassword() => _password;
+    public string GetEmail() => _user.Email;
+    public string GetAccessToken() => _tokens.Access;
+}
